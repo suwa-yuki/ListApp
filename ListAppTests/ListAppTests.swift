12bf2cc2b3393e801875dda819e54caeed165f24
Yuki Suwa <suwa.yuki@classmethod.jp>
2015-11-17 16:24:56 +0900

    [add] initialize project

---
+++ ListAppTests/ListAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  ListAppTests.swift
+//  ListAppTests
+//
+//  Created by Yuki on 2015/11/17.
+//  Copyright © 2015年 Yuki. All rights reserved.
+//
+
+import XCTest
+@testable import ListApp
+
+class ListAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
